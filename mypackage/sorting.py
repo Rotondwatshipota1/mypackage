600bfb2d1098715e2385dac362940dbbe0901c34
Rotondwatshipota1 <tshipotar@gmail.com>
2019-03-22 14:33:37 +0200

    Update sorting.py

--- mypackage/sorting.py
+++ mypackage/sorting.py
@@ -8,19 +8,77 @@ def bubble_sort(items):
 
     return out
 
- def merge_sort(items):
+def merge_sort(items):
 
-      x = len(items)
-       if not x: # length is 1
-            return items
-       else:
-            return merge_sort(merge_sort(items[:x]), merge_sort(items[x:]))
+    len_i = len(items)
+    # Base case. A list of size 1 is sorted.
+    # Cae returns, so if reached then function will terminate after line 8
+    if len_i == 1:
+        return items
+    # Recursive case. Find midpoint of list
+    mid_point = int(len_i / 2)
+    # divide list into two halves
+    i1 = merge_sort(items[:mid_point])
+    i2 = merge_sort(items[mid_point:])
+    # call merge_sort function on each half of list 
+    return merge(i1, i2)
 
-
-def quick_sort(items):
-        if len(items) <= 1:
-
-            return items
+# Merge function.
+def merge(A, B):
+    new_list = []
+    while len(A) > 0 and len(B) > 0:
+        if A[0] < B[0]:
+            new_list.append(A[0])
+            A.pop(0)
         else:
-            return quick_sort([x for x in items[1:]if x < items[0]])+
-            [items[0]]+ quick_sort([x for x in items[1:]if x >= items[0]])
+            new_list.append(B[0])
+            B.pop(0)
+
+    if len(A) == 0:
+        new_list = new_list + B
+    if len(B) == 0:
+        new_list = new_list + A
+
+    return new_list
+def quick_sort(items, index=-1):
+    """
+    the quick sort algorithm takes in an unsorted list of numbers.
+    returns a list in ascending order.
+
+    Parameters
+    ----------
+    items : list
+        list of unordered numbers
+    index: int, optional
+        index number at which to choose the split value
+        default set to the last item in the input list
+
+    Returns
+    -------
+    list
+        list of elements in items in ascending order
+    """
+    len_i = len(items)
+
+    if len_i <= 1:
+        # Logic Error
+        # identified with test run [1,5,4,3, 2, 6, 5, 4, 3, 8, 6, 5, 3, 1]
+        # len <= 1
+        return items
+
+    pivot = items[index]
+    small = []
+    large = []
+    dup = []
+    for i in items:
+        if i < pivot:
+            small.append(i)
+        elif i > pivot:
+            large.append(i)
+        elif i == pivot:
+            dup.append(i)
+
+    small = quick_sort(small)
+    large = quick_sort(large)
+
+    return small + dup + large
